a5eb6a9a84bfc64b5a6dd2b013e60ea77f6ca27a
Sarathsurendhran <sarathsurendhran15@gmail.com>
2024-08-03 15:17:54 +0530

    group chat not working  issue solved

--- frontend/src/components/user/chat/Chat.js
+++ frontend/src/components/user/chat/Chat.js
@@ -1,50 +1,107 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ChatTextEditor from "./ChatTextEditor";
+import { useSelector } from "react-redux";
+import { w3cwebsocket } from "websocket";
 
 const Chat = () => {
+  const groupId = useSelector((state) => state.group.groupId);
+  const webSocketURL = process.env.REACT_APP_webSocketURL;
+  const [chatMessages, setChatMessages] = useState([]);
+  const [connection, setConnection] = useState(null);
+  const chatContainerRef = useRef(null);
+  const { id, username } = useSelector((state) => state.authenticationUser);
+
+  useEffect(() => {
+    if (groupId) {
+      setChatMessages([]);
+      connectToWebSocket();
+    }
+    return () => {
+      if (connection) {
+        connection.close();
+      }
+    };
+  }, [groupId]);
+
+  // WebSocket connection
+  const connectToWebSocket = () => {
+
+    const newConnection = new w3cwebsocket(
+      `${webSocketURL}ws/group-chat/${groupId}/`
+    );
+    setConnection(newConnection);
+    newConnection.onopen = () => {
+      console.log("WebSocket client connected");
+    };
+    newConnection.onmessage = (message) => {
+      try {
+        const data = JSON.parse(message.data);
+
+        console.log("Received data:", data);
+
+        if (data.username && data.message) {
+          setChatMessages((prevMessages) => [...prevMessages, data]);
+          scrollToBottom();
+        } else {
+          console.error("Invalid message format:", data);
+        }
+      } catch (error) {
+        console.error("Error parsing message data:", error);
+      }
+    };
+
+    newConnection.onclose = () => {
+      console.log("WebSocket client disconnected, attempting to reconnect...");
+      setTimeout(connectToWebSocket, 2000); // Reconnect after 2 seconds
+    };
+
+    newConnection.onerror = (error) => {
+      console.error("WebSocket error:", error);
+      newConnection.close();
+    };
+  };
+
+  const scrollToBottom = () => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatMessages]);
+
   return (
     <>
-      <div className="h-screen bg-[#1f1e1f] bottom-8 flex ml-auto max-w-[76rem] flex-col overflow-y-scroll">
-        <div className=" flex-1 ">
-          <div className="px-4 py-2">
-            <div className="flex items-center mb-2 mt-36">
-              <img
-                className="w-8 h-8 rounded-full mr-2"
-                src="https://picsum.photos/50/50"
-                alt="User Avatar"
-              />
-              <div className="font-medium text-white ">John Doe</div>
-            </div>
-            <div className="bg-white rounded-lg p-2 shadow mb-2 max-w-sm">
-              Hi, how can I help you?
-            </div>
-            <div className="flex items-center text-white justify-end">
-              <div className="bg-blue-500 text-white rounded-lg p-2 shadow mr-2 max-w-sm">
-                Sure, I can help with that.
+      <div
+        className="h-screen bg-[#1f1e1f] flex ml-auto max-w-[76rem] flex-col overflow-y-scroll"
+        ref={chatContainerRef}
+      >
+        <div>
+          <div className="flex-1 px-4 py-2 mt-24 mb-28 max-h-full ">
+            {chatMessages.length === 0 && (
+              <div className="text-white">No messages yet...</div>
+            )}
+            {chatMessages.map((msg, index) => (
+              <div
+                key={index}
+                className={`mb-2 mr-4 flex ${
+                  msg.sender === id ? "justify-end" : "justify-start"
+                }`}
+              >
+                <div className="max-w-sm">
+                  <div className="text-white font-medium">{msg.username}</div>
+                  <div className="bg-gray-800 text-white rounded-lg p-2 shadow mb-2 text-lg">
+                    {msg.message}
+                  </div>
+                </div>
               </div>
-              <img
-                className="w-8 h-8 rounded-full"
-                src="https://picsum.photos/50/50"
-                alt="User Avatar"
-              />
-            </div>
+            ))}
           </div>
         </div>
-        {/* <div className="fixed bottom-0 left-0 right-0 bg-slate-700 border border-white px-4 py-2 max-w-[76rem] ml-auto">
-          <div className="flex items-center justify-between">
-          <input
-          className="w-full md:w-auto border rounded-full py-2 px-4 mr-2 mb-2 md:mb-0"
-          type="text"
-          placeholder="Type your message..."
-          />
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-full">
-          Send
-          </button>
-          </div>
-          </div> */}
-
-        <ChatTextEditor />
       </div>
+      <ChatTextEditor connection={connection} />
     </>
   );
 };
--- frontend/src/components/user/chat/ChatTextEditor.js
+++ frontend/src/components/user/chat/ChatTextEditor.js
@@ -1,65 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
+import { format } from "date-fns";
+import { useSelector } from "react-redux";
 
-import { useSelector, useDispatch } from "react-redux";
-
-// WebSocket imports
-import { w3cwebsocket as W3CwebSocket } from "websocket";
-
-const ChatTextEditor = () => {
-  const [message, setMessage] = useState("");
-  const [connection, setConnection] = useState(null);
-  const webSocketURL = process.env.REACT_APP_webSocketURL;
+const ChatTextEditor = ({ connection }) => {
   const groupId = useSelector((state) => state.group.groupId);
-
-  useEffect(() => {
-    if (groupId) {
-      connectToWebsocket();
-    }
-  }, [groupId]);
-
-  // websocket connection
-  const connectToWebsocket = () => {
-    const newConnection = new W3CwebSocket(
-      `${webSocketURL}ws/group-chat/${groupId}/`
-    );
-    setConnection(newConnection);
-    newConnection.onopen = () => {
-      console.log("websocket client connected");
-    };
-    newConnection.onmessage = (message) => {
-      const data = JSON.parse(message.data);
-      console.log(data);
-    };
-    return () => {
-      newConnection.close();
-    };
-  };
+  const { id, username } = useSelector((state) => state.authenticationUser);
+  const [message, setMessage] = useState("");
+  const inputRef = useRef(null);
 
   const sendMessage = (e) => {
     e.preventDefault();
-    if (message.trim()) {
-      console.log(message);
-      setMessage("");
+    if (!connection || connection.readyState !== connection.OPEN) {
+      console.log("Websocket is not open");
+      return;
     }
+    const currentTime = new Date();
+    const time = format(currentTime, "yyyy-MM-dd HH:mm:ss");
+
+    const messageData = {
+      message,
+      sender: id,
+      username,
+      time,
+    };
+
+    const messageString = JSON.stringify(messageData);
+    connection.send(messageString);
+
+    setMessage(""); // Clear the input after sending the message
+    inputRef.current.focus(); // Refocus on the input field
   };
-  
+  const isButtonDisabled = !message.trim();
 
   return (
-    <div className="fixed bottom-4 left-0 right-4 ml-auto max-w-[74rem] w-ful mt-4">
+    <div className="fixed bottom-4 left-0 right-4 ml-auto max-w-[74rem] w-ful mt-4 ">
       <form
         onSubmit={sendMessage}
         className="flex items-center p-4 bg-[#323232] rounded-md shadow-md"
       >
         <input
           type="text"
-          value={message}
+          value={message} // Control the input value with state
           onChange={(e) => setMessage(e.target.value)}
+          ref={inputRef}
           placeholder="Type your message..."
           className="flex-1 p-2 bg-[#323232] text-white rounded-md focus:outline-none focus:border-blue-500"
         />
 
-        <button className="flex items-center bg-blue-600 text-white gap-1 px-4 py-2 cursor-pointer  font-semibold tracking-widest rounded-md hover:bg-blue-500 ">
+        <button
+          className={`flex items-center gap-1 px-4 py-2 cursor-pointer font-semibold tracking-widest rounded-md ${
+            isButtonDisabled
+              ? "bg-gray-600 text-gray-400 cursor-not-allowed"
+              : "bg-blue-600 text-white hover:bg-blue-500"
+          }`}
+          disabled={isButtonDisabled}
+        >
           <SendIcon />
         </button>
       </form>
