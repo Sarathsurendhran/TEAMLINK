3516bd05df9101ba956774fe7715419f36a6540c
Sarathsurendhran <sarathsurendhran15@gmail.com>
2024-08-30 13:57:19 +0530

    celery work started

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -3,11 +3,20 @@ import os
 
 from celery import Celery
 from django.conf import settings
-from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_project.settings')
 
-app = Celery('django_celery_project')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+app = Celery("django_celery_project")
 app.conf.enable_utc = False
 
-app.conf.update(timezone = 'Asia/Kolkata')
+app.conf.update(timezone="Asia/Kolkata")
+
+app.config_from_object(settings, namespace="CELERY")
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f"Request:{self.request!r}")
