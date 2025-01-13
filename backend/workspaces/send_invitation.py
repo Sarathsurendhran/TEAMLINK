b38c5ba82131852ea91db31d0d7fef4dfc6cc4d6
Sarathsurendhran <sarathsurendhran15@gmail.com>
2025-01-13 10:30:16 +0530

    send invitation url changed

--- backend/workspaces/send_invitation.py
+++ backend/workspaces/send_invitation.py
@@ -6,7 +6,7 @@ from urllib.parse import urljoin
 def send_invitation(email, encrypted_data):
     try:
         email_from = settings.EMAIL_HOST_USER
-        base_url = "https://teamlinkconnect.online/register"
+        base_url = "https://teamlink-xi.vercel.app/register"
         invitation_link = urljoin(base_url, f"?join={encrypted_data}")
 
         # HTML content for the email
